2a134fb31a71d07fbf0933efb58628340ac9ec3a
Chanikya <chanikyaprakash@gmail.com>
2020-02-05 22:30:48 +0530

    adding login request to server

--- app.js
+++ app.js
@@ -11,7 +11,56 @@
 
 		$ctrl.htmlResult="None";
 		$ctrl.jsonResult="None";
+		$ctrl.accountStatus="";
 
+		$ctrl.createAccount = function(name,email,password){
+			var response=$http({
+				method:'POST',
+				url:("http://10.20.121.73:8000/login/"),
+				data: {'name':name,"email":email,"password":password}
+			});
+			response.then(function(data){
+				console.log("Response: ",data);
+				$ctrl.accountStatus=data.data;
+			})
+			.catch(function(error){
+				console.log("Failed to fetch details",error);	
+			});
+		};
+
+		$ctrl.sendDetails=function (type) {
+			var companies = [
+							{ 'name':'Infosys Technologies',
+								'employees': 125000,
+								'headoffice': 'Bangalore'},
+							{ 'name':'Cognizant Technologies',
+								'employees': 100000,
+								'headoffice': 'Bangalore'},
+							{ 'name':'Wipro',
+								'employees': 115000,
+								'headoffice': 'Bangalore'},
+							{ 'name':'Tata Consultancy Services (TCS)',
+								'employees': 150000,
+								'headoffice': 'Bangalore'},
+	                    ];
+			var response=$http({
+				method:type,
+				url:(ApiBasePath),
+				data: companies
+			});
+			response.then(function(data){
+				console.log("Response: ",data);
+				if (type=='PUT') {
+					$ctrl.PutStatus=data.data
+				}else if (type=='POST') {
+					$ctrl.PostStatus=data.data;
+				}
+			})
+			.catch(function(error){
+				console.log("Failed to fetch details",error);	
+			});
+
+		}
 		$ctrl.fetchDetails=function(type){
 			if (type=='html') {
 				type='';
@@ -20,21 +69,25 @@
 			}
 
 			var response=$http({
-				method:"GET",
+				// method:"HEAD",
 				url:(ApiBasePath+type)
 			});
-
-		response.then(function(data){
-			console.log("Response: ",data);
-			if (type=="") {
-				$ctrl.htmlResult=data.data;
-			}else{
-				$ctrl.jsonResult=data.data;
-			}
-		})
-		.catch(function(error){
-			console.log("Failed to fetch details",error);
-		});
+			response.then(function(data){
+				console.log("Response: ",data);
+				if (type=="") {
+					$ctrl.htmlResult=data.data;
+				}else{
+					$ctrl.jsonResult=data.data;
+				}
+			})
+			.catch(function(error){
+				console.log("Failed to fetch details",error);
+							if (type=="") {
+					$ctrl.htmlResult=error.data;
+				}else{
+					$ctrl.jsonResult=error.data;
+				}	
+			});
 
 		};
 	}
